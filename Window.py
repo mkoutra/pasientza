386265ae6a545b9eddde8e73d3cdd796a5fc3824
mkoutra <koutra98@hotmail.com>
2024-02-12 10:26:28 +0200

    Progress in GameWindow class
    - Better use of anchor and sticky arguments
    - imgs directory created.
    - The eight canvas where placed in a frame.

--- Window.py
+++ Window.py
@@ -3,45 +3,77 @@ import os
 import tkinter as tk
 from PIL import Image, ImageTk
 
+from Cards import Card
+
 class GameWindow:
     def __init__(self):
-        #TODO make arguments optional
-        self._dimensions = (1024, 600)
-        self._background = "green"
+        # Window configuration
+        self._win_dimensions = (1024, 800)
+        self._background = "darkgreen"
         self._title = "Pasientza"
-        self.img_folder = "card_imgs"
+        self._card_img_folder = os.path.join("imgs", "card_imgs")
+
+        # Card configuration
+        self._card_dimensions = (100, 130)
+
+        # Maps card id (e.g. "10s") to card image
+        self._card_images:dict = {}
+
+        # Collection of 8 canvas used to draw the SuitDecks
+        self._all_SuitDeck_canvas:list = []
 
         # Create root window
         self._root = tk.Tk()
-        self._root.geometry(str(self._dimensions[0]) + "x"
-                            + str(self._dimensions[1]))
-        self._root.configure(background = self._background)
         self._root.title(self._title)
+        self._root.configure(background = self._background)
+        self._root.geometry(str(self._win_dimensions[0]) + "x"
+                            + str(self._win_dimensions[1]))
 
-        # Add blank image
-        # for i in range(8):
-            # self.draw_card("blank_img.jpg", 0, i)
+        # Frame containing the eight SuitDecks
+        self._suitDecks_frame = tk.Frame(master = self._root,
+                                         bg = self._background)
+        self._suitDecks_frame.place(relx = 0, rely = 0.02, anchor = tk.NW)
+
+        # Load Playing Cards
+        for fname in os.listdir(self._card_img_folder):
+            img = Image.open(os.path.join(self._card_img_folder ,fname))
+            img = img.resize(self._card_dimensions)
+
+            card_name = fname.strip(".png")
+            self._card_images[card_name] = ImageTk.PhotoImage(img)
+        
+        # Load the Blank card
+        img = Image.open(os.path.join("imgs" ,"Blank_img.jpg"))
+        img = img.resize(self._card_dimensions)
+        self._card_images["Blank"] = ImageTk.PhotoImage(img)
+
+        # Load the Blue playing card
+        img = Image.open(os.path.join("imgs" ,"Blue_playing_card.jpg"))
+        img = img.resize(self._card_dimensions)
+        self._card_images["Blue"] = ImageTk.PhotoImage(img)
+
+        # Create the eight SuitDecks with Blank Image on front.
+        for i in range(8):
+            canv = tk.Canvas(master = self._suitDecks_frame,
+                             width = self._card_dimensions[0],
+                             height = self._card_dimensions[1])
+
+            canv.grid(row = 0, column = i, padx = 10, sticky=tk.NW)
+            
+            canv.create_image(0, 0, anchor = tk.NW, 
+                              image = self._card_images["Blue"])
+
+            self._all_SuitDeck_canvas.append(canv)
 
     def draw_card(self, card_id:str, row, col):
-        img = find_card(os.path.join(self.img_folder, card_id))
+        img = find_card(os.path.join(self._card_img_folder, card_id))
         lbl = tk.Label(master = self._root, image = img)
         lbl.image = img
         lbl.grid(row = row, column = col, padx = 5, pady = 5)
 
-    def draw_deck(self):
-        """It prints 8 columns of 13 cards overlapping in a column"""
-        image = find_card(os.path.join(self.img_folder, "As.png"))
-        for j in range(8):
-            canvas = tk.Canvas(self._root, width = image.width(), height = image.height(), bg="green")
-            canvas.grid(row = 0, column = j, padx = 10)
-            canvas.image = image
-            # Set the overlap distance (adjust as needed)
-            overlap_x = 0
-            overlap_y = 35
-            
-            # Call the function to overlap and display images
-            for i in range(13):
-                overlap_images(canvas, image, i, overlap_x, overlap_y)
+    def draw_card_in_suitDeck(self, deck_id:int, card:Card, i:int):
+        card_img = self._card_images[card.id()]        
+        overlap_images(self._all_SuitDeck_canvas[deck_id], card_img, i, 0, 35)
 
     def draw(self):
         """Draw window"""
@@ -63,8 +95,7 @@ def find_card(cardname:str):
 
     return ImageTk.PhotoImage(img)
 
-def overlap_images(canvas, image, i, overlap_x, overlap_y):
-    # Get the size of the images
+def overlap_images(canvas, image, i:int, overlap_x, overlap_y):
     width, height = image.width(), image.height()
 
     # Calculate the total size of the canvas
@@ -72,14 +103,31 @@ def overlap_images(canvas, image, i, overlap_x, overlap_y):
     canvas_height = max(height, (height + i*overlap_y))
 
     # Create a Canvas widget with the calculated size
-    canvas.config(width=canvas_width, height=canvas_height)
+    canvas.config(width = canvas_width, height = canvas_height)
 
-    # Draw an image at position (i*overlap_x, i*overlap_y)
-    canvas.create_image(i*overlap_x, i*overlap_y, anchor=tk.NW, image=image)
+    # Draw an image at position (overlap_x, i*overlap_y) measured from anchor point
+    canvas.create_image(overlap_x, i * overlap_y, anchor=tk.NW, image=image)
 
 if __name__ == "__main__":
+    c1 = Card('10', 's')
+    c2 = Card('8', 'd')
+    c3 = Card('A', 's')
     win = GameWindow()
 
-    win.draw_deck()
-    win.draw_card("3s.png", 0, 0)
+    # win.draw_card_in_suitDeck(0, c1, 0)
+    # win.draw_card_in_suitDeck(0, c2, 1)
+    # win.draw_card_in_suitDeck(0, c3, 2)
+    for i in range(13):
+        win.draw_card_in_suitDeck(0, c3, i)
+
+    # win.draw_card_in_suitDeck(1, c3, 0)
+    # win.draw_card_in_suitDeck(3, c2, 0)
+    # win.draw_card_in_suitDeck(3, c1, 1)
+
+    # win.draw_deck()
+    # k = 0
+    # while (True):
+        # xarti = input("xarti: ")
+        # win.draw_card(xarti + ".png", k, k)
+        # k += 1
     win.draw()
--- test_printing.py
+++
@@ -1,6 +0,0 @@
-a = [[1 ,2, 3], [4, 5]]
-
-for row in range(2):
-    for col in range(3):
-        print(a[row][col], end=" ")
-    print("\n")
